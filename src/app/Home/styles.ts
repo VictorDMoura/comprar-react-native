ab8035f631e7944c3c4ba084b800b846d6512361
VictorDMoura <mouramvictor@gmail.com>
2026-01-12 21:40:28 -0300

    feat: add Input component and integrate it into Home with updated styles

--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -5,12 +5,25 @@ export const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     backgroundColor: "#d0d2d8",
-    paddingTop: 62,
-    padding: 24,
-    gap: 24
+    paddingTop: 62
   },
   logo: {
     height: 34,
     width: 134,
   },
+  form: {
+    paddingHorizontal: 16,
+    width: "100%",
+    gap: 7,
+    marginTop: 42
+  },
+  content: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+    padding: 24,
+    marginTop: 24
+  }
 });
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    height: 48,
+    width: "100%",
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    fontSize: 14,
+    borderWidth: 1,
+    borderColor: "#c3c5cb",
+  },
+});
